76bc87b1249204d7690c4eda51b9b990d4a3ef31
Tarun sharma <tarun555sharma55@gmail.com>
2025-06-17 09:06:55 +0530

    Add files via upload

---
+++ # 01_hello_tarun.py
@@ -0,0 +1,26 @@
+# 01_hello_tarun.py
+
+"""
+This is my first Python script as part of my AI journey.
+Created by: Tarun Saharma
+"""
+
+# Basic print
+print("Hello, world! 👋")
+print("I'm Tarun, starting my journey to become a Deep Learning Engineer.")
+
+# Variables
+my_name = "Tarun"
+goal = "Deep Learning Engineer"
+current_stage = "Learning Python basics"
+
+print("\nAbout me:")
+print("My name is", my_name)
+print("My goal is to become a", goal)
+print("Right now, I'm at the stage of:", current_stage)
+
+# A simple math operation
+x = 10
+y = 5
+print("\nMath check: 10 + 5 =", x + y)
+print("\n 10-5 =" , x-y)
